3940f5b8683b67eae60b3af8c83537056dedfc2e
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-07-30 22:23:43 +0700

    Add validation to api/post/mobil.php

--- api/post/mobil.php
+++ api/post/mobil.php
@@ -16,6 +16,26 @@ $harga_sewa = $_POST['harga_sewa'];
 $foto = $_FILES['foto'];
 $status = 'Bebas'; //default : bebas
 
+/*
+| ----------------------------------------------------------------------------------
+| Input Validation
+| ---------------------------------------------------------------------------------- */
+
+if (! validate_alphanumeric_only($plat_nomor)) 	validasiGagal('Plat nomor harus berisi huruf dan angka saja.');
+if (! validate_number_only($no_rangka)) 		validasiGagal('No. Rangka harus berisi angka saja.');
+if (! validate_number_only($no_mesin))			validasiGagal('No. Mesin harus berisi angka saja.');
+if (! validate_name_only($nama))				validasiGagal('Nama Mobil harus berisi huruf, spasi dan titik saja.');
+if (! validate_name_only($jenis))				validasiGagal('Jenis Mobil harus berisi huruf, spasi dan titik saja.');
+if (! validate_alphanumeric_only($merk))		validasiGagal('Merk Mobil harus berisi huruf, spasi dan titik saja.');
+if (! validate_enum($warna, ['merah', 'biru', 'hitam', 'abu'])) validasiGagal('Warna Mobil harus berisi Merah, Biru, Hitam, Abu.');
+if (! validate_format($tahun, 'dddd'))			validasiGagal('Tahun harus berformat YYYY');
+if (! validate_number_only($harga_sewa)) 		validasiGagal('Harga Sewa harus berisi angka saja.');
+
+kirimPesanJikaValidasiGagal();
+
+// --------------------------------------------------------------------------------- //
+
+
 //check image
 $check = file_exists($foto['tmp_name']);
 
